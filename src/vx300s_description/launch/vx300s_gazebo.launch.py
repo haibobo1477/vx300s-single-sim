d0eab721e1d6f44c4fb29b5835187ed3f68f4096
haibo <zhaohaibo177417@gmail.com>
2025-09-15 15:51:34 -0500

    wrong

--- src/vx300s_description/launch/vx300s_gazebo.launch.py
+++ src/vx300s_description/launch/vx300s_gazebo.launch.py
@@ -1,77 +1,57 @@
-from launch import LaunchDescription
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
+import launch
+import launch.event_handlers
+import launch.launch_description_sources
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+import os
+
+import launch_ros.parameter_descriptions
+
+
 
 def generate_launch_description():
-    # 1. Xacro -> robot_description
-    xacro_file = PathJoinSubstitution([
-        FindPackageShare('vx300s_description'),
-        'urdf',
-        'vx300s.xacro'
-    ])
-    robot_description = Command([FindExecutable(name='xacro'), ' ', xacro_file])
-
-    # 2. RViz 配置
-    rviz_config_file = PathJoinSubstitution([
-        FindPackageShare('vx300s_description'),
-        'config',
-        'vx300s_display.rviz'
-    ])
-
-    # 3. Gazebo world 文件 (你保存的 world 放在 vx300s_description/worlds 目录里)
-    world_file = PathJoinSubstitution([
-        FindPackageShare('vx300s_description'),
-        'world',
-        'custom_room.world'   # ⚠️ 这里替换成你保存的 world 名字
-    ])
-
-    return LaunchDescription([
-        # robot_state_publisher
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            parameters=[{'robot_description': robot_description}],
-            output='screen'
+    
+    urdf_package_path = get_package_share_directory('vx300s_description')
+    default_xacro_path = os.path.join(urdf_package_path, 'urdf', 'vx300s.xacro')
+    # default_rviz_config_path = os.path.join(urdf_package_path, 'config', 'display_robot_model.rviz')
+    default_gazebo_world_path = os.path.join(urdf_package_path, 'world', 'custom_room.world')
+    
+    # 添加参数名为model:= xxxxxxxxx
+    action_declare_arg_mode_path = launch.actions.DeclareLaunchArgument(
+        name='model',default_value=str(default_xacro_path),
+        description='加载模型文件.'
+    )
+    
+    substitutions_command_result = launch.substitutions.Command(['xacro ', launch.substitutions.LaunchConfiguration('model')])
+    
+    robot_description_value = launch_ros.parameter_descriptions.ParameterValue(substitutions_command_result, value_type=str)
+    
+    action_robot_state_publisher = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description':robot_description_value}]
+    )
+    
+    action_launch_gazebo = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            [get_package_share_directory('gazebo_ros'), '/launch', '/gazebo.launch.py']
         ),
-
-        # joint_state_publisher
-        Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            name='joint_state_publisher',
-            output='screen'
-        ),
-
-        # RViz2
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_file],
-            output='screen'
-        ),
-
-        # Gazebo (加载 world)
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                FindPackageShare('gazebo_ros'),
-                '/launch',
-                '/gazebo.launch.py'
-            ]),
-            launch_arguments={'world': world_file}.items()
-        ),
-
-        # Spawn robot into Gazebo
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            arguments=[
-                '-topic', 'robot_description',
-                '-entity', 'vx300s'
-            ],
-            output='screen'
-        )
+        # launch_arguments=[{'world', default_gazebo_world_path},{'verbose','true'}]
+        launch_arguments={'world': default_gazebo_world_path,
+                  'verbose': 'true'}.items()
+    )
+    
+    action_spawn_entity = launch_ros.actions.Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', '/robot_description', '-entity', 'vx300s']
+    )
+    
+ 
+    
+    return launch.LaunchDescription([
+        action_declare_arg_mode_path,
+        action_robot_state_publisher,
+        action_launch_gazebo,
+        action_spawn_entity,  
     ])
